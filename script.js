85496929a32d8c84a0252b745e82142655d86945
kiethohoai <kietho.060193@gmail.com>
2024-09-25 22:28:59 +0700

    #5 Values and Variables

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+let firstName = "John";
+let lastName = "Ho";
+
+console.log(firstName + " " + lastName);
+console.log(`${firstName} ${lastName}`);
+console.log(`Hello, my name is ${firstName} ${lastName}.`);
+
+let job1 = "Programmer";
+let job2 = "Teacher";

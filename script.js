74d62b9bb699ebe9e9d4d2972d18d79b5fd28593
kiethohoai <kietho.060193@gmail.com>
2024-09-26 09:52:46 +0700

    003 Functions

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 'use strict';
 
-// const interface = 'Audio';
-const private = 123;
+// function logger() {
+//   console.log(`My name is John`);
+// }
+
+// logger();
+
+// function test(a, b) {
+//   console.log('🚀  a, b =>', a, b);
+//   return `Juice with ${a} apples and ${b} oranges!`;
+// }
+// console.log('🚀  test(5, 0) =>', test(5, 0));

d19155b04da7b3c267b36689ec5de63ccd3d48f2
kiethohoai <kietho.060193@gmail.com>
2024-09-26 09:00:08 +0700

    022 The switch Statement

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-const a = false;
-const b = false;
+const day = 'sunday';
 
-console.log('🚀  a =>', a);
-console.log('🚀  b =>', b);
-
-console.log('🚀  a && b =>', a && b);
-console.log('🚀  a || b =>', a || b);
-console.log('🚀  !a && !b =>', !a && !b);
+switch (day) {
+  case 'monday':
+    console.log('Plan course structure');
+    console.log('Prepare theory videos');
+    break;
+  case 'tuesday':
+    console.log('Write code examples');
+    break;
+  case 'wednesday':
+  case 'thursday':
+    console.log('Record videos');
+    break;
+  case 'friday':
+    console.log('Record videos');
+    console.log('Ask questions');
+    break;
+  case 'saturday':
+  case 'sunday':
+    console.log('Enjoy');
+    break;
+  default:
+    console.log('Not a valid day');
+}

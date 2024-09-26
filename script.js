3b66d34360937f51a5473d2f5654e3ab6ff62b0c
kiethohoai <kietho.060193@gmail.com>
2024-09-26 15:48:05 +0700

    007 Reviewing Functions

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const calcAge = function (birthYear) {
+  const age = 2037 - birthYear;
+  return age;
+};
+
+const year = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(year(1991, 'Jonas'));

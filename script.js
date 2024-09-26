076faa7e37a5b5d5db13f39c0ea07849dfd9b215
kiethohoai <kietho.060193@gmail.com>
2024-09-26 16:37:32 +0700

    012 Objects

--- script.js
+++ script.js
@@ -1,34 +1,7 @@
-const friends = ['John', 'Ahmed', 'Sayed', 'Eman'];
-console.log('🚀  friends =>', friends);
+const johnHo = {
+  name: 'John',
+  age: 100 - 70,
+  address: 'Hue City',
+};
 
-// // push => add a new element in the end
-// const newLength = friends.push('Osama');
-// console.log('🚀  friends =>', friends);
-
-// // unshift =>  add a new element in the beginning
-// const newLength2 = friends.unshift('John');
-// console.log('🚀  friends =>', friends);
-
-// // pop =>  remove the last element of array
-// const popped = friends.pop();
-// console.log('🚀  friends =>', friends);
-// console.log('🚀  popped =>', popped);
-
-// // shift() remove the first element
-// friends.shift();
-// console.log('🚀  friends =>', friends);
-
-// // indexOf
-// const index = friends.indexOf('Ahmed');
-// console.log('🚀  index =>', index);
-
-// ncludes() return true/false
-
-// const isIncludes = friends.includes('John');
-// console.log('🚀  isIncludes =>', isIncludes);
-
-// if (friends.includes('John')) {
-//   console.log('John is included');
-// } else {
-//   console.log('John is not included');
-// }
+console.log('🚀  johnHo =>', johnHo);

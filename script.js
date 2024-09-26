87061ddfad7b0074ab03c71955b47e53f0bab7f6
kiethohoai <kietho.060193@gmail.com>
2024-09-26 17:31:40 +0700

    014 Object Methods

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
-//013 Dot vs. Bracket Notation
+// 014 Object Methods
 
-const john = {
-  name: 'John',
-  age: 32,
-  city: 'Miami',
-  friends: ['Michael', 'Steven', 'Peter'],
+const person = {
+  firstName: 'John',
+  lastName: 'Ho',
+  birthYear: 1994,
+  age: undefined,
+
+  getAge: function () {
+    return (this.age = 2024 - this.birthYear);
+  },
+
+  getSummary: function () {
+    return `${this.firstName} ${this.lastName} is ${this.getAge()} years old.`;
+  },
 };
 
-// console.log('🚀  john =>', john);
-// console.log('🚀  john.name =>', john['name']);
-// console.log('🚀  john.age =>', john['age']);
-// console.log('🚀  john.city =>', john['city']);
-
-// const data = prompt("What do you want to know about John? Choose between 'name', 'age' or 'city'");
-
-// if (john[data]) {
-//   console.log('🚀  john[data] =>', john[data]);
-// } else {
-//   console.log('Wrong request! Choose between "name", "age" or "city"');
-// }
-
-// john.address = 'Thua Thien Hue';
-// john['address'] = 'Thua Thien Hue';
-// const address = 'address';
-// john[address] = 'Thua Thien Hue';
-// console.log('🚀  john =>', john);
-
-console.log(
-  `John have ${john.friends.length} friends and his best friend is ${john.friends[0]}.`,
-);
+console.log('🚀  person =>', person);
+console.log('🚀  person.getSummary() =>', person.getSummary());

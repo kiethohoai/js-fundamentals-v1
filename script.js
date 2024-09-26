8f4b3f684dc62c9df440e91e6e7dae87d9f5a65d
kiethohoai <kietho.060193@gmail.com>
2024-09-26 07:48:40 +0700

    014 Taking Decisions if  else Statements

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const t1 = 1;
-const t2 = 2;
-const isTrue = t2 >= t1;
-console.log(isTrue);
+const age = 17;
+const isOldEnough = age >= 18;
+
+if (isOldEnough) console.log('Sarah can start driving license now.');
+else console.log(`Sarah is too young. Wait another ${18 - age} years.`);

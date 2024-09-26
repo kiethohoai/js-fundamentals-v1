0218ec67e8b42220c0da4a7575c29b8ca739fcfc
kiethohoai <kietho.060193@gmail.com>
2024-09-26 10:16:33 +0700

    005 Arrow Functions

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
 'use strict';
-
-const r1 = calAge1(1997);
-console.log('🚀  r1 =>', r1);
-
-// Function Declaration
-function calAge1(birthYear) {
-  return 2037 - birthYear;
-}
-
-// Expressing Function
-const calAge2 = function (birthYear) {
-  return 2037 - birthYear;
-};
-
-const r2 = calAge2(1994);
-console.log('🚀  r2 =>', r2);
+const a = () => console.log('Hello World');
+a();

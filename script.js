df1da73f22dfdfe628604d8bf2f9fd3749b1d422
kiethohoai <kietho.060193@gmail.com>
2024-09-26 18:07:52 +0700

    017 Looping Arrays, Breaking and Continuing

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-// 016 Iteration The for Loop
+// 017 Looping Arrays, Breaking and Continuing
 
-for (let i = 0; i < 10; i++) {
-  console.log(i);
-}
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// 0. for loop
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log(numbers[i]);
+// }
+
+// 1. forEach()
+// 2. map()
+// 3. filter()
+// 4. find()
+// 5. findIndex()
+// 6. some()
+// 7. every()
+// 8. reduce()

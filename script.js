ac0efce0d74b94982334c1b29e9fbcd606a40f19
kiethohoai <kietho.060193@gmail.com>
2024-09-26 09:16:46 +0700

    024 The Conditional (Ternary) Operator

--- script.js
+++ script.js
@@ -1,25 +1,2 @@
-const day = 'sunday';
-
-switch (day) {
-  case 'monday':
-    console.log('Plan course structure');
-    console.log('Prepare theory videos');
-    break;
-  case 'tuesday':
-    console.log('Write code examples');
-    break;
-  case 'wednesday':
-  case 'thursday':
-    console.log('Record videos');
-    break;
-  case 'friday':
-    console.log('Record videos');
-    console.log('Ask questions');
-    break;
-  case 'saturday':
-  case 'sunday':
-    console.log('Enjoy');
-    break;
-  default:
-    console.log('Not a valid day');
-}
+const a = 10 > 5 ? true : false;
+console.log('🚀  a =>', a);

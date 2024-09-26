60175a65eb965aad10a477804b480eb25277aed1
kiethohoai <kietho.060193@gmail.com>
2024-09-26 20:47:49 +0700

    004 Installing Node.js and Setting Up a Dev Environment

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// 018 Looping Backwards and Loops in Loops
-// const numbers = [1, 2, 3, 4, 5];
-
-// let i = 1;
-// while (i <= 10) {
-//   console.log('i =', i);
-//   i++;
-// }
-
-let dice = Math.trunc(Math.random() * 6) + 1;
-
-while (dice !== 6) {
-  console.log('You rolled a', dice);
-  dice = Math.trunc(Math.random() * 6) + 1;
-
-  if (dice === 6) console.log('Loop is about to end...');
-}
+console.log("Ho Hoai Kiet");
+console.log("Ho Hoai Kiet");
+console.log("Ho Hoai Kiet");

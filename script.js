db1d2107de93411aafcdd99f07d0c9179b05cd7a
kiethohoai <kietho.060193@gmail.com>
2024-09-27 08:26:57 +0700

    009 Debugging with the Console and Breakpoints

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-console.log("Ho Hoai Kiet");
-console.log("Ho Hoai Kiet");
-console.log("Ho Hoai Kiet");
+const mskv = function () {
+  const ms = {
+    type: 'temp',
+    unit: 'c',
+    value: +prompt('What is the temperature in Celsius?'),
+  };
+  console.warn('🚀  ms.value =>', ms.value);
+
+  debugger;
+  const kv = ms.value + 273;
+  return kv;
+};
+console.warn('🚀  mskv() =>', mskv());

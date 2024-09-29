b4640e871b8698ceaada3b06aeef52b0fe00c8ff
kiethohoai <kietho.060193@gmail.com>
2024-09-29 17:14:32 +0700

    017 Switching the Active Player

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 'use strict';
 
 // todo Selecting Elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const current0El = document.querySelector('#current--0');
@@ -16,7 +19,11 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
 let currentScore = 0;
+const scores = [0, 0];
+let activePlayer = 0;
+let playing = true;
 
 //todo Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -31,8 +38,13 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   } else {
     // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });

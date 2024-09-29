f54cea994a6dbafd96cca5b695973ce397f338f6
kiethohoai <kietho.060193@gmail.com>
2024-09-29 13:33:29 +0700

    009 Coding Challenge #1

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 'use strict';
 
-let score = 2;
+let score = 20;
+let highScore = 0;
 document.querySelector('.score').textContent = score;
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.warn(`🚀  secretNumber =>`, secretNumber);
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -17,8 +18,14 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.score').textContent = score;
 
     // Change background color
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   } else if (guess > secretNumber) {
     if (score === 0) {
       document.querySelector('.message').textContent = '🚀YOU LOSE🚀';
@@ -36,4 +43,21 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = score;
     }
   }
+});
+
+const againButton = document.querySelector('.again');
+againButton.addEventListener('click', function () {
+  document.querySelector('.message').textContent = 'Start guessing...';
+
+  score = 20;
+  document.querySelector('.score').textContent = score;
+
+  document.querySelector('.guess').value = '';
+
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  console.warn(`🚀  secretNumber =>`, secretNumber);
+
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
 });

1cb807a5ac515665e2518f626134a83742860bb6
kiethohoai <kietho.060193@gmail.com>
2024-10-01 09:24:05 +0700

    014 Primitives vs. Objects in Practice

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+// todo E1 Primitive Types
+// let lName = 'John';
+// let lName2 = lName;
+// lName = 'David';
+
+// console.log(`🚀  lName =>`, lName);
+// console.log(`🚀  lName2 =>`, lName2);
+
+// todo E2 Reference Types
+const john = {
+  name: 'John',
+  age: 30,
+};
+
+const david = Object.assign({}, john);
+david.name = 'David';
+
+console.log(`🚀  john =>`, john);
+console.log(`🚀  david =>`, david);

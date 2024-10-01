5847fbd9ad26f99082be861e7791ae57baddb333
kiethohoai <kietho.060193@gmail.com>
2024-10-01 09:13:03 +0700

    013 Primitives vs. Objects (Primitive vs. Reference Types)

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
 'use strict';
-
-const john = function test(a, b) {
-  console.log(arguments);
-  console.log(`🚀  a + b =>`, a + b);
-};
-
-john(1, 2);
-
-const john2 = (a, b) => {
-  console.log(arguments);
-  console.log(`🚀  a + b =>`, a + b);
-};
-
-john2(1, 2);

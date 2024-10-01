09c622e53c502923c21e8c4602fee9c035a315f0
kiethohoai <kietho.060193@gmail.com>
2024-10-01 08:57:16 +0700

    012 Regular Functions vs. Arrow Functions

--- script.js
+++ script.js
@@ -1,29 +1,15 @@
 'use strict';
 
-// console.log(this);
-
-// const calcAge = function (birthYear) {
-//   console.log(2037 - birthYear);
-//   console.log(this);
-// };
-// calcAge(1994);
-
-// const calcAge = birthYear => {
-//   console.log(2037 - birthYear);
-//   console.log(this);
-// };
-// calcAge(1994);
-
-// const john = {
-//   year: 1994,
-//   calcAge: function () {
-//     console.log(this);
-//     // console.log(2024 - this.year);
-//   },
-// };
-
-// john.calcAge();
-
-const john = {
-  year: 1994,
+const john = function test(a, b) {
+  console.log(arguments);
+  console.log(`🚀  a + b =>`, a + b);
 };
+
+john(1, 2);
+
+const john2 = (a, b) => {
+  console.log(arguments);
+  console.log(`🚀  a + b =>`, a + b);
+};
+
+john2(1, 2);

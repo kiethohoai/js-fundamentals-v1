f87ad4ca1fb738c8bea021c3b959110e1d0a2647
kiethohoai <kietho.060193@gmail.com>
2024-10-01 10:23:49 +0700

    004 Destructuring Objects

--- script.js
+++ script.js
@@ -6,14 +6,25 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
 };
 
-// Destructuring
-const arr = [1, 2, 3];
-const [a, b, c] = arr;
-// console.log(a, b, c);
+const {
+  fri: { open, close },
+} = restaurant.openingHours;
 
-const [first, , second] = restaurant.categories;
-console.log(first, second);
-
-
+console.log(open, close);

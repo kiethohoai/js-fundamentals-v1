1cf4e096664e00f02f3c6ae67b56b9cd106c1b0e
kiethohoai <kietho.060193@gmail.com>
2024-10-02 14:38:22 +0700

    016 Sets

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
 'use strict';
 
-const jonas = {
-  firstName: 'Jonas',
-  lastName: 'Schmedtmann',
-  age: 2037 - 1991,
-  job: 'teacher',
-  friends: ['Michael', 'Peter', 'Steven'],
-};
+const staff = "hohoaikiet";
 
-const temp = Object.keys(jonas);
-console.log(`🚀  temp =>`, temp);
-
-const temp2 = Object.values(jonas);
-console.log(`🚀  temp =>`, temp2);
-
-const temp3 = Object.entries(jonas);
-console.log(`🚀  temp =>`, temp3);
-
-for (let [key, value] of temp3) {
-  console.log(`🚀Key:Value => ${key} : ${value}`);
-}
+const newStaff = new Set(staff);
+console.log(`🚀  newStaff =>`, newStaff)

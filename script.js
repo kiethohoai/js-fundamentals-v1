4fbca8102c6f9a8bd794817e51956602f01a4fb8
kiethohoai <kietho.060193@gmail.com>
2024-10-02 08:32:28 +0700

    011 Looping Arrays The for-of Loop

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let item of arr) {
+//   if (item === 2) continue;
+//   console.log(`🚀  item =>`, item);
+// }
+
+for (let item of arr.entries()) {
+  console.log(`🚀  item =>`, item);
+}
+
+console.log(...arr.entries());

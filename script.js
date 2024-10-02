9f3cdc97d9e666636f3720cf947befe0ba86442d
kiethohoai <kietho.060193@gmail.com>
2024-10-02 08:21:06 +0700

    009 Logical Assignment Operators

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 'use strict';
 
-// AND
-const test = null;
-const temp = test ?? 1000;
-console.log(`🚀  temp =>`, temp)
+const r1 = {
+  name: 'Mina',
+  numGuest: 0,
+};
+
+const r2 = {
+  name: 'MiNi',
+  owner: 'Giorgio Rossi',
+};
+
+r1.numGuest = r1.numGuest ??= 10;
+r2.numGuest = r2.numGuest ??= 10;
+
+console.log(r1);
+console.log(r2);

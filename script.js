83bb7c4addbf1bdc705e43e960aa726d15c67daf
kiethohoai <kietho.060193@gmail.com>
2024-10-02 15:21:39 +0700

    021 Working With Strings - Part 1

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 'use strict';
 
-const rest = new Map();
-rest.set('name', 'Classico Italiano').set(1, 'Firenze, Italy').set(2, 'Lisbon, Portugal');
-console.log(`🚀  rest =>`, rest);
+const airline = 'Vietnam Airline';
+const plane = 'Boeing 737';

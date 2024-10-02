06e4d19e6b6607bf80f6a55afd42f10b5f35fc3a
kiethohoai <kietho.060193@gmail.com>
2024-10-02 21:12:38 +0700

    022 Working With Strings - Part 2

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 'use strict';
 
-const airline = 'Vietnam Airline';
-const plane = 'Boeing 737';
+// const john = 'jOhn';
+// const res = john[0].toLocaleUpperCase() + john.slice(1).toLocaleLowerCase();
+// console.log(`🚀  res =>`, res);
+
+// const name = '   Ho Hoai Kiet    ';
+// console.log(name.trim());
+
+// const price = '$100';
+// price.replace('$', 'USD');
+// console.log(`🚀  price.replace('$', 'USD') =>`, price.replace('$', 'USD'))
+
+// const name = 'ho hoai kiet ho hoai san ho hoai nam';
+// console.log(name.replaceAll('ho', 'Hồ'));
+
+// const name = 'ho hoai kiet ho hoai san ho hoai nam';
+// console.log(name.replace(/ho hoai/g, 'Hồ Hoài'));
+
+const name = 'Ho Hoai Kiet';
+console.log(name.includes('Kiet'));
+console.log(name.startsWith('Ho'));

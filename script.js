2a31b6ff744dce24591fbbdedc21d04e67c2a117
kiethohoai <kietho.060193@gmail.com>
2024-10-02 08:23:04 +0700

    010 Coding Challenge #1

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
 'use strict';
-
-const r1 = {
-  name: 'Mina',
-  numGuest: 0,
-};
-
-const r2 = {
-  name: 'MiNi',
-  owner: 'Giorgio Rossi',
-};
-
-r1.numGuest = r1.numGuest ??= 10;
-r2.numGuest = r2.numGuest ??= 10;
-
-console.log(r1);
-console.log(r2);

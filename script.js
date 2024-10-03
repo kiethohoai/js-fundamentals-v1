6aa1efce42f491938872762deaf448a8a00752c8
kiethohoai <kietho.060193@gmail.com>
2024-10-03 16:11:36 +0700

    005 First-Class and Higher-Order Functions

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
 'use strict';
-
-const flight = 'AA123';
-const jonas = {
-  name: 'Jonas Schmedtmann',
-  passport: 24739479284,
-};
-
-const checkIn = function (flightNum, passenger) {
-  flightNum = 'LH999';
-  passenger.name = 'Mr. ' + passenger.name;
-
-  if (passenger.passport === 24739479284) alert('Checked in');
-  else alert('Wrong passport!');
-};
-checkIn('AA123', jonas);
-console.log(`🚀  flight =>`, flight);
-console.log(`🚀  jonas =>`, jonas);

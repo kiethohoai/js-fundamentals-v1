16c5e1187e7a1863997b24bc1a3d95b87b3f0056
kiethohoai <kietho.060193@gmail.com>
2024-10-03 15:46:59 +0700

    003 Default Parameters

--- script.js
+++ script.js
@@ -1,31 +1,12 @@
 'use strict';
 
-// const john = 'jOhn';
-// const res = john[0].toLocaleUpperCase() + john.slice(1).toLocaleLowerCase();
-// console.log(`🚀  res =>`, res);
+const test = function (name = 'John', age = '30', address = 'Hue') {
+  const test2 = {
+    name,
+    age,
+    address,
+  };
+  console.log(`🚀  test2 =>`, test2);
+};
 
-// const name = '   Ho Hoai Kiet    ';
-// console.log(name.trim());
-
-// const price = '$100';
-// price.replace('$', 'USD');
-// console.log(`🚀  price.replace('$', 'USD') =>`, price.replace('$', 'USD'))
-
-// const name = 'ho hoai kiet ho hoai san ho hoai nam';
-// console.log(name.replaceAll('ho', 'Hồ'));
-
-// const name = 'ho hoai kiet ho hoai san ho hoai nam';
-// console.log(name.replace(/ho hoai/g, 'Hồ Hoài'));
-
-// const name = 'Ho Hoai Kiet';
-// console.log(name.includes('Kiet'));
-// console.log(name.startsWith('Ho'));
-
-// const name = 'Ho+Hoai+Kiet';
-// console.log(name.split('+').join(" "));
-
-// const arr = ['Ho', 'Hoai', 'Kiet', 'Ho', 'Hoai', 'San', 'Ho', 'Hoai', 'Nam'];
-// console.log(arr.join(' '));
-
-const name = 'Ho Hoai Kiet';
-console.log(name.padStart(20, '*').padEnd(30, '*'));
+test();

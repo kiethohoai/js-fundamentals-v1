c52b9a661784ec99fef5bf34c20bb3ce4de0ada6
kiethohoai <kietho.060193@gmail.com>
2024-10-03 16:44:21 +0700

    007 Functions Returning Functions

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+
+// const result = greet('Hey');
+// result('John');
+// result('Jonas');
+
+// greet('Hello')("David");
+
+const greet = greeting => name => console.log(`${greeting} ${name}`);
+
+const result = greet('Hello');
+result('John');
+result('Jonas');
+greet('Hello')('David');

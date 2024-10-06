8e5a348535c5c73b6d9ef3f317b2b5318e1a5338
kiethohoai <kietho.060193@gmail.com>
2024-10-06 10:17:53 +0700

    003 Simple Array Methods

---
+++ 05/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/* let f;
+
+const g = function () {
+  const x = 23;
+
+  f = function () {
+    console.log(x * 2);
+  };
+};
+
+const h = function () {
+  const z = 7;
+  f = function () {
+    console.log(z * 2);
+  };
+};
+
+g();
+f();
+h();
+f(); */

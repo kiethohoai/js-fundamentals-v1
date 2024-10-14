4ab56babff3cc3e22e8c394bed191b2defee5ba9
kiethohoai <kietho.060193@gmail.com>
2024-10-14 09:52:24 +0700

    004 Math and Rounding

--- script.js
+++ script.js
@@ -85,6 +85,7 @@ inputLoginPin.value = '1';
 /////////////////////////////////////////////////
 // Functions
 
+// Display Movements
 const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
 
@@ -96,7 +97,7 @@ const displayMovements = function (movements, sort = false) {
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,17 +105,19 @@ const displayMovements = function (movements, sort = false) {
   });
 };
 
+// calcDisplayBalance
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
+// calcDisplaySummary
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements.filter(mov => mov > 0).reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -124,9 +127,10 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
+// createUsernames
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
     acc.username = acc.owner
@@ -138,6 +142,7 @@ const createUsernames = function (accs) {
 };
 createUsernames(accounts);
 
+// updateUI
 const updateUI = function (acc) {
   // Display movements
   displayMovements(acc.movements);
@@ -195,10 +200,11 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+// Request loan
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = +inputLoanAmount.value;
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -241,7 +247,3 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
-
-// 003 Converting and Checking Numbers
-console.log(parseInt('2.5px'));
-console.log(parseFloat('2.5px'));
